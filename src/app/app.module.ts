6af8ca1c11402f88fbf7d0a430ebdb9870cfa38b
Daniel Uno <danielunosoto@gmail.com>
2024-06-02 11:02:56 -0400

    add: added user state.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/state-management/app.state.ts
@@ -0,0 +1,10 @@
+import { UserState } from "./states/user.state";
+
+export interface ConnectingState {
+  users: UserState,
+  // groups: GroupState
+  // clients: ClientState
+  // addresses: AddressState
+  // assistances: AssistanceState
+  // workOrders: WorkOrderState
+}
